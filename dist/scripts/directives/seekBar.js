b470e9c8ad4e6b6509738af90736769498da2e08
Forrest Stone <iloveburningman@gmail.com>
2016-12-21 21:14:36 -0800

    chapter 9 done

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -31,6 +31,15 @@
                          width: percentString()
                      };
                  };
+                 
+                 scope.thumbStyle = function(){
+                     
+                     return{
+                         left: percentString()
+                     };
+                     
+                 }
+                 
                  scope.onClickSeekBar = function(event) {
                      var percent = calculatePercent(seekBar, event);
                      scope.value = percent * scope.max;
