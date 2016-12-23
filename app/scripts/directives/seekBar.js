40a8e523996d188cd41dc56cd1d719aad71cfaa4
Forrest Stone <iloveburningman@gmail.com>
2016-12-22 17:07:16 -0800

    half way done with follow along

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -12,12 +12,29 @@
              templateUrl: '/templates/directives/seek_bar.html',
              replace: true,
              restrict: 'E',
-             scope: {},
+             scope: {
+                onChange: '&'
+             },
              link: function(scope, element, attributes) {
                  scope.value = 0;
                  scope.max = 100;
 
                  var seekBar = $(element);
+                 
+                 
+                  attributes.$observe('value', function(newValue) {
+                        scope.value = newValue;
+                  });
+ 
+                  attributes.$observe('max', function(newValue) {
+                        scope.max = newValue;
+                  });
+                 
+                 
+                 //my work dont think i needed to do this
+//                 attributes.$$observe('on-change', function(newValue){
+//                          
+//                 })
 
                  var percentString = function() {
                      var value = scope.value;
@@ -43,14 +60,22 @@
                  scope.onClickSeekBar = function(event) {
                      var percent = calculatePercent(seekBar, event);
                      scope.value = percent * scope.max;
+                     notifyOnChange(scope.value);
                  };
                  scope.trackThumb = function() {
                      $document.bind('mousemove.thumb', function(event) {
                          var percent = calculatePercent(seekBar, event);
                          scope.$apply(function() {
                              scope.value = percent * scope.max;
+                             notifyOnChange(scope.value);
                          });
                      });
+                     
+                     var notifyOnChange = function(newValue) {
+     if (typeof scope.onChange === 'function') {
+         scope.onChange({value: newValue});
+     }
+ };
 
                      $document.bind('mouseup.thumb', function() {
                          $document.unbind('mousemove.thumb');
--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,22 +1,28 @@
 (function() {
-     function SongPlayer(Fixtures) {
-        
+    function SongPlayer(Fixtures) {
+
         var SongPlayer = {};
-         
-         
-         //Did I write this right?
-          /**
+
+
+        //Did I write this right?
+        /**
          * @atribute currentAlbum
          * @desc assigns the result of the function Fixtures.getAlbum() the the varible currentAlbum
          */
-         
-         var currentAlbum = Fixtures.getAlbum();
 
-//        SongPlayer.currentSong = null; moved down not sure about the move or what why we changed it from  var SongPlayer.currentSong = null;
+        var currentAlbum = Fixtures.getAlbum();
+
+        //        SongPlayer.currentSong = null; moved down not sure about the move or what why we changed it from  var SongPlayer.currentSong = null;
         /**
          * @desc Buzz object audio file
-         * @type {Object}
+         * @type {number}
          */
+
+        /**
+         * @desc Current playback time (in seconds) of currently playing song
+         * @type {Number}
+         */
+        SongPlayer.currentTime = null;
         var currentBuzzObject = null;
 
         /**
@@ -38,28 +44,28 @@
 
             SongPlayer.currentSong = song;
         };
-        
-        
-      //1st try
-        
-        var playSong = function(song){
+
+
+        //1st try
+
+        var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
         }
 
-        
-         /**
+
+        /**
          * @function getSongIndex
          * @desc converts the currently playoing song and returns a new object
          */
-        
-         var getSongIndex = function(song) {
-             return currentAlbum.songs.indexOf(song);
-         };
-        
-          //moved from above
+
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+
+        //moved from above
         SongPlayer.currentSong = null;
-        
+
         SongPlayer.play = function(song) {
             song = song || SongPlayer.currentSong;
             if (SongPlayer.currentSong !== song) {
@@ -69,37 +75,43 @@
             }
         };
 
-        
-        SongPlayer.stopSong = function(){
+
+        SongPlayer.stopSong = function() {
             currentBuzzObject.stop();
             SongPlayer.currentSong.playing = null;
-            
+
         }
-        
+
         SongPlayer.pause = function(song) {
             song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
         };
-         
-         SongPlayer.next = function(){
+
+        SongPlayer.next = function() {
             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
             currentSongIndex++;
             if (currentSongIndex >= currentAlbum.songs.length) {
-                
+
                 SongPlayer.stopSong();
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
                 playSong(song);
             }
-             
-         }
+
+        }
+
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
 
         SongPlayer.previous = function() {
             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
             currentSongIndex--;
-            
+
             if (currentSongIndex < 0) {
                 SongPlayer.stopSong();
                 //SongPlayer.currentSong.playing = null;
@@ -111,7 +123,7 @@
         };
         return SongPlayer;
 
-        
+
     }
 
     angular
